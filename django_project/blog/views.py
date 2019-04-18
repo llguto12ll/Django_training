8a23d3c375f44dc4969efcb46254930d734c02b1
llguto12ll <49766671+llguto12ll@users.noreply.github.com>
2019-04-18 20:00:13 -0300

    Update views.py

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -6,4 +6,4 @@ def home(request):
     return render(request,'blog/home.html')
 
 def about(request):
-    return render(request,'blog/about.html')
+    return render(request,'blog/about.html')#testemermerge
